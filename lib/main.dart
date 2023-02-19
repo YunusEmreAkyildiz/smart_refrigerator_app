ae89f0cbe2e20a2a180bfd01be9616c20659208d
Yunus Emre Akyıldız <y.emreakyildiz53@gmail.com>
2023-02-20 01:08:41 +0300

    Firebase connection created
    -Connected to Firebase Cloud Firestore DB and successfully made write operations with a physical device.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
+final FirebaseFirestore _dbInstance = FirebaseFirestore.instance;
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'));
   }
 }
 
@@ -54,6 +59,15 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            ElevatedButton(
+              onPressed: () => dataAdd(),
+              child: const Text('Data Add (no doc ID)'),
+            ),
+            ElevatedButton(
+              onPressed: () => dataSet(),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: const Text('Data Set (uses doc ID)'),
+            ),
           ],
         ),
       ),
@@ -64,4 +78,39 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  void dataAdd() async {
+    Map<String, dynamic> _userMap = {
+      'name': 'yunus',
+      'age': 24,
+      'isStudent': true,
+      'adress': {'city': 'Istanbul', 'province': 'Maltepe'},
+      'colors': FieldValue.arrayUnion(['Blue', 'Green']),
+      'createdAt': FieldValue.serverTimestamp()
+    };
+    await _dbInstance.collection('users').add(_userMap);
+    _incrementCounter();
+  }
+}
+
+// void dataAdd(var counter) async {
+//   setState(() {
+//       counter++;
+//     });
+//   _incrementCounter();
+//   Map<String, dynamic> _userMap = {
+//     'name': 'yunus',
+//     'age': 24,
+//     'isStudent': true,
+//     'adress': {'city': 'Istanbul', 'province': 'Maltepe'},
+//     'colors': FieldValue.arrayUnion(['Blue', 'Green']),
+//     'createdAt': FieldValue.serverTimestamp()
+//   };
+//   await _dbInstance.collection('users').add(_userMap);
+// }
+
+void dataSet() async {
+  await _dbInstance.doc('users/ux7f02f1VOR3LNBWdNnR').set(
+      {'university': 'Istanbul University - Cerrahpasa'},
+      SetOptions(merge: true));
 }
